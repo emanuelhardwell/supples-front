189c341d757846f7d97f22d8b5ae4f2b4f178675
emanuelhardwell <emanuelnoe16@gmail.com>
2022-07-09 10:28:38 -0500

    [Store-actions] se crearon acciones asyncronas startLogin, startRegister, startAuthCheckingFinish, startLogout

---
+++ src/store/slices/auth/authThunk.js
@@ -0,0 +1,57 @@
+import Swal from "sweetalert2";
+import { fetchWithOutToken, fetchWithToken } from "../../../helpers/fetch";
+import { authCheckingFinish, authLogin, authLogout } from "./authSlice";
+
+export const startLogin = (user) => {
+  return async (dispatch) => {
+    const res = await fetchWithOutToken("auth/login", user, "POST");
+    const body = await res.json();
+
+    if (body.ok) {
+      localStorage.setItem("token", body.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+      dispatch(authLogin({ uid: body.uid, name: body.name }));
+    } else {
+      Swal.fire("Error", body.message, "error");
+    }
+  };
+};
+
+export const startRegister = (user) => {
+  return async (dispatch) => {
+    const res = await fetchWithOutToken("auth/create", user, "POST");
+    const body = await res.json();
+
+    if (body.ok) {
+      localStorage.setItem("token", body.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+      dispatch(authLogin({ uid: body.uid, name: body.name }));
+    } else {
+      Swal.fire("Error", body.message, "error");
+    }
+  };
+};
+
+export const startAuthCheckingFinish = () => {
+  return async (dispatch) => {
+    const res = await fetchWithToken("auth/renew");
+    const body = await res.json();
+
+    if (body.ok) {
+      localStorage.setItem("token", body.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+      dispatch(authLogin({ uid: body.uid, name: body.name }));
+    } else {
+      // Swal.fire("Error", body.message, "error");
+      dispatch(authCheckingFinish());
+    }
+  };
+};
+
+export const startLogout = () => {
+  return (dispatch) => {
+    localStorage.clear();
+    dispatch(authLogout());
+    // dispatch(postLogout());
+  };
+};
