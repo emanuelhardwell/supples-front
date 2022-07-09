7279a278491caffc7eb4d05ee3fbe4df56c50c2e
emanuelhardwell <emanuelnoe16@gmail.com>
2022-07-09 10:25:46 -0500

    [Redux] se configuro el Store usando Redux-Toolkit

---
+++ src/store/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { authSlice } from "./slices/auth/authSlice";
+
+export const store = configureStore({
+  reducer: { auth: authSlice.reducer },
+});
