9f7fe19ecb389ab70d2bf4a8720e9a478f69a15e
emanuelhardwell <emanuelnoe16@gmail.com>
2022-07-09 10:45:22 -0500

    [Component] se actualizaron las dependencias

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { AppRouter } from "./routers/AppRouter";
+import { store } from "./store/store";
+
+export const App = () => {
+  return (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  );
+};
