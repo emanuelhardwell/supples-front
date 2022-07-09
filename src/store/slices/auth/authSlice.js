2ff96174d6d2900a998123bc2b581927558d0c93
emanuelhardwell <emanuelnoe16@gmail.com>
2022-07-09 10:26:53 -0500

    [Redux-slice] se creo el authSlice

---
+++ src/store/slices/auth/authSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    // counter: 10,
+    checking: true,
+    // name: null,
+    // uid: null,
+  },
+  reducers: {
+    // increment: (state /* action */) => {
+    //   state.counter += 1;
+    // },
+    authCheckingFinish: (state) => {
+      state.checking = false;
+    },
+    authLogin: (state, action) => {
+      state.checking = false;
+      state.name = action.payload.name;
+      state.uid = action.payload.uid;
+    },
+    authLogout: (state) => {
+      state.checking = false;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { authCheckingFinish, authLogin, authLogout } = authSlice.actions;
+export default authSlice.reducer;
