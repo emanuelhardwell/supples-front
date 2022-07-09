899bf9c2c2c6d1e516e5c6dc05402711abb995c8
emanuelhardwell <emanuelnoe16@gmail.com>
2022-07-09 10:23:17 -0500

    [Helper] se creo helperFetch con las funcionalidades fetchWithOutToken, fetchWithToken, fetchWithTokenAndFile

---
+++ src/helpers/fetch.js
@@ -0,0 +1,63 @@
+const urlBase = process.env.REACT_APP_API;
+
+export const fetchWithOutToken = (endpoint, data, method = "GET") => {
+  const url = `${urlBase}/${endpoint}`;
+
+  if (method === "GET") {
+    return fetch(url);
+  } else {
+    return fetch(url, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  }
+};
+
+export const fetchWithToken = (endpoint, data, method = "GET") => {
+  const url = `${urlBase}/${endpoint}`;
+
+  const token = localStorage.getItem("token") || "";
+
+  if (method === "GET") {
+    return fetch(url, {
+      headers: {
+        token: token,
+      },
+    });
+  } else {
+    return fetch(url, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+        "x-token": token,
+      },
+      body: JSON.stringify(data),
+    });
+  }
+};
+
+export const fetchWithTokenAndFile = (endpoint, data, method = "GET") => {
+  const url = `${urlBase}/${endpoint}`;
+
+  const token = localStorage.getItem("token") || "";
+
+  if (method === "GET") {
+    return fetch(url, {
+      headers: {
+        token: token,
+      },
+    });
+  } else {
+    return fetch(url, {
+      method,
+      headers: {
+        // "Content-Type": "multipart/form-data",
+        "x-token": token,
+      },
+      body: data,
+    });
+  }
+};
