8dd8964e3e8da8caaabe90cef771579a7cfa401e
emanuelhardwell <emanuelnoe16@gmail.com>
2022-07-09 10:32:07 -0500

    [Component] se crearon los componentes de Login y Register

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,168 @@
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import { Link } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { useState } from "react";
+import {
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { Link as LinkRouter } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import Swal from "sweetalert2";
+import { startLogin } from "../../store/slices/auth/authThunk";
+
+const Copyright = (props) => {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link
+        color="inherit"
+        href="https://www.instagram.com/emanuel.hardwell"
+        target="_blank"
+      >
+        Emanuel Hardwell
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+};
+
+const initialState = {
+  email: "",
+  password: "",
+};
+
+export const Login = () => {
+  const dispatch = useDispatch();
+  const [formValues, setFormValues] = useState(initialState);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const { email, password } = formValues;
+
+  const handleInputChange = ({ target }) => {
+    setFormValues({ ...formValues, [target.name]: target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email.trim() === "" || password.trim() === "") {
+      return Swal.fire("Error", "Ingresa el correo y la contraseña", "error");
+    }
+    dispatch(startLogin(formValues));
+  };
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Iniciar sesión
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Correo"
+              name="email"
+              value={email}
+              onChange={handleInputChange}
+            />
+
+            <FormControl
+              sx={{ width: "fullWidth" }}
+              margin="normal"
+              fullWidth
+              variant="outlined"
+              required
+            >
+              <InputLabel htmlFor="outlined-adornment-password">
+                Contraseña
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                name="password"
+                value={password}
+                onChange={handleInputChange}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      //   onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Contraseña"
+              />
+            </FormControl>
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Iniciar sesión
+            </Button>
+
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  ¿Se te olvidó tu contraseña?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link component={LinkRouter} to="/register" variant="body2">
+                  {"Crear una cuenta"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </>
+  );
+};
---
+++ src/components/auth/Register.jsx
@@ -0,0 +1,239 @@
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import { Link } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { useState } from "react";
+import {
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { Link as LinkRouter } from "react-router-dom";
+import Swal from "sweetalert2";
+import { useDispatch } from "react-redux";
+import { startRegister } from "../../store/slices/auth/authThunk";
+
+const Copyright = (props) => {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link
+        color="inherit"
+        href="https://www.instagram.com/emanuel.hardwell"
+        target="_blank"
+      >
+        Emanuel Hardwell
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+};
+
+const initialState = {
+  name: "",
+  lastname: "",
+  lastname2: "",
+  email: "",
+  password: "",
+  password2: "",
+};
+
+export const Register = () => {
+  const dispatch = useDispatch();
+  const [formValues, setFormValues] = useState(initialState);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const { name, lastname, lastname2, email, password, password2 } = formValues;
+
+  const handleInputChange = ({ target }) => {
+    setFormValues({ ...formValues, [target.name]: target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      name.trim() === "" ||
+      lastname.trim() === "" ||
+      lastname2.trim() === "" ||
+      email.trim() === ""
+    ) {
+      return Swal.fire("Error", "Ingresa todos los datos", "error");
+    }
+
+    if (password !== password2) {
+      return Swal.fire("Error", "Las contraseñas no coinciden", "error");
+    }
+    dispatch(startRegister(formValues));
+  };
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Crea tu cuenta
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Nombre"
+              name="name"
+              value={name}
+              onChange={handleInputChange}
+            />
+
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Apellido paterno"
+              name="lastname"
+              value={lastname}
+              onChange={handleInputChange}
+            />
+
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Apellido materno"
+              name="lastname2"
+              value={lastname2}
+              onChange={handleInputChange}
+            />
+
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Correo"
+              name="email"
+              value={email}
+              onChange={handleInputChange}
+            />
+
+            <FormControl
+              sx={{ width: "fullWidth" }}
+              margin="normal"
+              fullWidth
+              variant="outlined"
+              required
+            >
+              <InputLabel htmlFor="outlined-adornment-password">
+                Contraseña
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                name="password"
+                value={password}
+                onChange={handleInputChange}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      //   onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Contraseña"
+              />
+            </FormControl>
+
+            <FormControl
+              sx={{ width: "fullWidth" }}
+              margin="normal"
+              fullWidth
+              variant="outlined"
+              required
+            >
+              <InputLabel htmlFor="outlined-adornment-password2">
+                Repetir contraseña
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password2"
+                type={showPassword ? "text" : "password"}
+                name="password2"
+                value={password2}
+                onChange={handleInputChange}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      //   onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Repetir contraseña"
+              />
+            </FormControl>
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Registrarse
+            </Button>
+
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link component={LinkRouter} to="/login" variant="body2">
+                  {"¿Ya tienes una cuenta? Iniciar sesión"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </>
+  );
+};
