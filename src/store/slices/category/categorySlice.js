bfd3dffb0f5365df6f2c018107a0e415208f3530
emanuelhardwell <emanuelnoe16@gmail.com>
2022-07-12 14:13:37 -0500

    [Slice] se agrego el categorySlices y sus Actions

---
+++ src/store/slices/category/categorySlice.js
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const categorySlice = createSlice({
+  name: "category",
+  initialState: {
+    categories: [],
+    categoryActive: null,
+    modalOpen: false,
+  },
+  reducers: {
+    categoryGet: (state, action) => {
+      state.categories = action.payload;
+    },
+    categoryAdd: (state, action) => {
+      state.categories.push(action.payload);
+    },
+    categoryDelete: (state, action) => {
+      state.categories = state.categories.filter(
+        (category) => category.id !== action.payload
+      );
+    },
+    categoryUpdate: (state, action) => {
+      state.categories = state.categories.map((category) =>
+        category.id === action.payload.id ? action.payload : category
+      );
+    },
+    categoryModalOpen: (state) => {
+      state.modalOpen = true;
+    },
+    categoryModalClose: (state) => {
+      state.modalOpen = false;
+    },
+    categorySetActive: (state, action) => {
+      state.categoryActive = action.payload;
+    },
+    categoryClearActive: (state) => {
+      state.categoryActive = null;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const {
+  categoryGet,
+  categoryAdd,
+  categoryDelete,
+  categoryUpdate,
+  categoryModalClose,
+  categoryModalOpen,
+  categoryClearActive,
+  categorySetActive,
+} = categorySlice.actions;
+export default categorySlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./slices/auth/authSlice";
+import { categorySlice } from "./slices/category/categorySlice";
 
 export const store = configureStore({
-  reducer: { auth: authSlice.reducer },
+  reducer: { auth: authSlice.reducer, category: categorySlice.reducer },
 });
