6d610c4310bbf4d8c6c0c2dbf081d841d55d66a9
emanuelhardwell <emanuelnoe16@gmail.com>
2022-07-13 22:46:02 -0500

    [Action] se crearon las acctions asyncronas de product

---
+++ src/store/slices/product/productThunk.js
@@ -0,0 +1,87 @@
+import { toast } from "react-toastify";
+import Swal from "sweetalert2";
+import { fetchWithToken, fetchWithTokenAndFile } from "../../../helpers/fetch";
+import {
+  productAdd,
+  productDelete,
+  productGet,
+  productGetCategories,
+  productUpdate,
+} from "./productSlice";
+
+export const startProductGet = () => {
+  return async (dispatch) => {
+    const res = await fetchWithToken("product", {}, "GET");
+    const body = await res.json();
+
+    if (body.ok) {
+      dispatch(productGet(body.products));
+    } else {
+      Swal.fire("Error", body.message, "error");
+    }
+  };
+};
+
+export const startProductAdd = (product) => {
+  return async (dispatch) => {
+    const res = await fetchWithTokenAndFile("product", product, "POST");
+    const body = await res.json();
+
+    if (body.ok) {
+      dispatch(productAdd(body.product));
+      toast(body.message, {
+        type: "success",
+        autoClose: 4000,
+      });
+    } else {
+      Swal.fire("Error", body.message, "error");
+    }
+  };
+};
+
+export const startProductDelete = (id) => {
+  return async (dispatch) => {
+    const res = await fetchWithToken(`product/${id}`, {}, "DELETE");
+    const body = await res.json();
+
+    if (body.ok) {
+      dispatch(productDelete(id));
+      toast(body.message, {
+        type: "success",
+        autoClose: 4000,
+      });
+    } else {
+      Swal.fire("Error", body.message, "error");
+    }
+  };
+};
+
+export const startProductUpdate = (product, id) => {
+  return async (dispatch) => {
+    const res = await fetchWithTokenAndFile(`product/${id}`, product, "PUT");
+    const body = await res.json();
+
+    if (body.ok) {
+      dispatch(productUpdate(body.product));
+      toast(body.message, {
+        type: "success",
+        autoClose: 4000,
+      });
+    } else {
+      Swal.fire("Error", body.message, "error");
+    }
+  };
+};
+
+export const startProductGetCategories = () => {
+  return async (dispatch) => {
+    const res = await fetchWithToken("category", {}, "GET");
+    const body = await res.json();
+
+    if (body.ok) {
+      dispatch(productGetCategories(body.categories));
+    } else {
+      Swal.fire("Error", body.message, "error");
+    }
+  };
+};
