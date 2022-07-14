123f49b360c742e52541d9ffaffdf683f5af3d73
emanuelhardwell <emanuelnoe16@gmail.com>
2022-07-14 18:50:29 -0500

    [Component] se agrego la funcionalidad de paginación

--- src/components/admin/product/ProductScreen.jsx
+++ src/components/admin/product/ProductScreen.jsx
@@ -1,22 +1,37 @@
-import { Container, CssBaseline, Grid } from "@mui/material";
+import {
+  Container,
+  CssBaseline,
+  Grid,
+  Pagination,
+  PaginationItem,
+} from "@mui/material";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  startProductGet,
+  startProductGetByPopulation,
   startProductGetCategories,
 } from "../../../store/slices/product/productThunk";
 import { Navbar } from "../../ui/Navbar";
 import { ProductCard } from "./ProductCard";
 import { ProductForm } from "./ProductForm";
+import { useLocation, Link } from "react-router-dom";
 
 export const ProductScreen = () => {
   const dispatch = useDispatch();
-  const { products } = useSelector((state) => state.product);
+  const { products, currentPage, numberOfPages } = useSelector(
+    (state) => state.product
+  );
+  const location = useLocation();
+  const useQuery = () => {
+    return new URLSearchParams(location.search);
+  };
+  const query = useQuery();
+  const page = query.get("page") || 1;
 
   useEffect(() => {
-    dispatch(startProductGet());
+    dispatch(startProductGetByPopulation(page));
     dispatch(startProductGetCategories());
-  }, [dispatch]);
+  }, [dispatch, page]);
 
   return (
     <>
@@ -31,6 +46,26 @@ export const ProductScreen = () => {
           {products?.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
+        </Grid>
+
+        <Grid container sx={{ py: 2 }}>
+          <Grid item xs={12}>
+            {numberOfPages && currentPage && (
+              <Pagination
+                sx={{ display: "flex", justifyContent: "center" }}
+                count={numberOfPages}
+                page={currentPage}
+                color="primary"
+                renderItem={(item) => (
+                  <PaginationItem
+                    component={Link}
+                    to={`${item.page === 1 ? "" : `?page=${item.page}`}`}
+                    {...item}
+                  />
+                )}
+              />
+            )}
+          </Grid>
         </Grid>
       </Container>
     </>
