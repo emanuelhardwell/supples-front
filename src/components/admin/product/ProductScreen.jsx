d018f4146158a929d997d8df562f0c10dc59340d
emanuelhardwell <emanuelnoe16@gmail.com>
2022-07-23 12:09:02 -0500

    [Component] se actualizo ProductScreen

--- src/components/admin/product/ProductScreen.jsx
+++ src/components/admin/product/ProductScreen.jsx
@@ -65,14 +65,23 @@ export const ProductScreen = () => {
       <Container maxWidth="xl">
         <Grid container sx={{ pt: 3 }}>
           <Grid item xs={12} sm={6}>
-            <ProductForm />
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: { xs: "center", sm: "center" },
+              }}
+            >
+              <ProductForm />
+            </Box>
           </Grid>
+
           <Grid item xs={12} sm={6}>
             <Box
               sx={{
                 display: "flex",
                 flexDirection: "column",
-                alignItems: "center",
+                alignItems: { xs: "center", sm: "center" },
               }}
             >
               <Box
