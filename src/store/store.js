256aa9ddbc11c5fcc36309c2f034aaa756dbc3d7
emanuelhardwell <emanuelnoe16@gmail.com>
2022-07-28 17:35:01 -0500

    [Redux-store] se agrego el Slice de Cart al Store

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./slices/auth/authSlice";
+import { cartSlice } from "./slices/cart/cartSlice";
 import { categorySlice } from "./slices/category/categorySlice";
 import { productSlice } from "./slices/product/productSlice";
 
@@ -8,5 +9,6 @@ export const store = configureStore({
     auth: authSlice.reducer,
     category: categorySlice.reducer,
     product: productSlice.reducer,
+    cart: cartSlice.reducer,
   },
 });
