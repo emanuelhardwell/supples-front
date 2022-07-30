295725f5bc38dbc171673115eb9725166b014fd5
emanuelhardwell <emanuelnoe16@gmail.com>
2022-07-29 21:46:59 -0500

    [Component] se actualizo

--- src/components/cart/CartScreen.jsx
+++ src/components/cart/CartScreen.jsx
@@ -13,26 +13,96 @@ import TableRow from "@mui/material/TableRow";
 const TAX_RATE = 0.07;
 
 function ccyFormat(num) {
-  return `${num.toFixed(2)}`;
+  let number = Number(num);
+  return `${number.toFixed(2)}`;
 }
 
-function priceRow(quantity, price) {
-  return quantity * price;
-}
+// function priceRow(quantity, price) {
+//   return quantity * price;
+// }
 
-function createRow(desc, quantity, price) {
-  const sum = priceRow(quantity, price);
-  return { desc, quantity, price, sum };
-}
+// function createRow(desc, quantity, price) {
+//   const sum = priceRow(quantity, price);
+//   return { desc, quantity, price, sum };
+// }
 
 function subtotal(items) {
   return items.map(({ sum }) => sum).reduce((sum, i) => sum + i, 0);
 }
 
 const rows = [
-  createRow("Paperclips (Box)", 100, 1.15),
-  createRow("Paper (Case)", 10, 45.99),
-  createRow("Waste Basket", 2, 17.99),
+  {
+    name: "Giant Sports Performance Series 100% Whey Isolate | 25 gr de Proteína Completa Derivada únicamente de Whey Protein Isolate 90% WPI |Solo 2 g de Carbohidratos",
+    price: "649.01",
+    imageUrl:
+      "https://res.cloudinary.com/emanuel-hardwell/image/upload/v1658628631/supples/dzbebpzqlge2cxrauqx8.jpg",
+    CartItem: {
+      id: 2,
+      cartId: 2,
+      productId: 1,
+      quantity: 1,
+    },
+  },
+  {
+    name: "FORZAGEN | Proteína Forzawhey-Pro | 100% Whey Protein | 5 lb (2,27 kg) | 69 Servicios x Envase | Delicioso Sabor a Chocolate | 25 g de Proteína de Suero por Servicio | Bajo en Carbohidratos",
+    price: "1289.00",
+    imageUrl:
+      "https://res.cloudinary.com/emanuel-hardwell/image/upload/v1658628813/supples/oewfe9vnedqbavi9pvra.jpg",
+    CartItem: {
+      id: 1,
+      cartId: 2,
+      productId: 2,
+      quantity: 2,
+    },
+  },
+  {
+    name: "Glutamina | Glutamine | 90 porciones de 5 g c/u | 100% glutamina | Sin saborizantes | Sin excipientes | Post entrenamiento",
+    price: "635.00",
+    imageUrl:
+      "https://res.cloudinary.com/emanuel-hardwell/image/upload/v1658630109/supples/bhk5ljpv0homefg3oi8z.jpg",
+    CartItem: {
+      id: 4,
+      cartId: 2,
+      productId: 14,
+      quantity: 5,
+    },
+  },
+  {
+    name: "German American Technology Mezcla de Proteínas y Aminoácidos Glutamine, 300 g",
+    price: "449.00",
+    imageUrl:
+      "https://res.cloudinary.com/emanuel-hardwell/image/upload/v1658630196/supples/xtbcllbvrjkwnsu4mx0h.jpg",
+    CartItem: {
+      id: 5,
+      cartId: 2,
+      productId: 16,
+      quantity: 3,
+    },
+  },
+  {
+    name: "L-CARNITINE CAPSULE 1,200 100 caps",
+    price: "399.00",
+    imageUrl:
+      "https://res.cloudinary.com/emanuel-hardwell/image/upload/v1658630578/supples/av9zbzvmb78asq65xfof.jpg",
+    CartItem: {
+      id: 3,
+      cartId: 2,
+      productId: 21,
+      quantity: 4,
+    },
+  },
+  {
+    name: "L-Carnitina Platinum 1000 mg |4 en 1| +Vinagre de Manzana | +CLA | +Cetonas de frambuesa | Testrol | 90 Caps",
+    price: "423.47",
+    imageUrl:
+      "https://res.cloudinary.com/emanuel-hardwell/image/upload/v1658630687/supples/bjsadv4gqhrpjzlnqwrc.jpg",
+    CartItem: {
+      id: 6,
+      cartId: 2,
+      productId: 23,
+      quantity: 3,
+    },
+  },
 ];
 
 const invoiceSubtotal = subtotal(rows);
@@ -69,12 +139,14 @@ export const CartScreen = () => {
                   </TableHead>
                   <TableBody>
                     {rows.map((row) => (
-                      <TableRow key={row.desc}>
-                        <TableCell>{row.desc}</TableCell>
-                        <TableCell align="right">{row.quantity}</TableCell>
+                      <TableRow key={row.name}>
+                        <TableCell>{row.name}</TableCell>
+                        <TableCell align="right">
+                          {row?.CartItem?.quantity}
+                        </TableCell>
                         <TableCell align="right">{row.price}</TableCell>
                         <TableCell align="right">
-                          {ccyFormat(row.sum)}
+                          {ccyFormat(row.price)}
                         </TableCell>
                       </TableRow>
                     ))}
